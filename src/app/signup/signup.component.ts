24aec2ea9136b8428720672e19188abd4aa604fa
maaz khan <maazkhaansofteng@gmail.com>
2023-10-12 15:51:47 +0500

    adding sign up

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocialAuthService, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  showForm = false;
 
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+  constructor(private authService: SocialAuthService) {}
+  signIn(){}
+  loginWithGoogle() {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID)
+      .then((user: SocialUser) => {
+        // Handle user data from Google Sign-In
+        alert("logein bbvmnb")
+      });
+  // loginWithGoogle(): void {
+  //   // this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+  loginWithFacebook(){
+
+  }
 }
